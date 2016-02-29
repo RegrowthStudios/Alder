b621fceb6e5e659897608b3838064447320145de
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-02-29 01:18:08 +0000

    Add email to license header.

--- library/Sycamore/Row/User.php
+++ library/Sycamore/Row/User.php
@@ -1,7 +1,7 @@
   <?php
 
 /* 
- * Copyright (C) 2016 Matthew Marshall
+ * Copyright (C) 2016 Matthew Marshall <matthew.marshall96@yahoo.co.uk>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

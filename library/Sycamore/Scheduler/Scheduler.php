1b4a25f83038c8979d6e6b439cd86a60e2cdfd4e
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-03-03 01:10:59 +0000

    Add functionality for adding tasks to OS.

--- library/Sycamore/Scheduler/Scheduler.php
+++ library/Sycamore/Scheduler/Scheduler.php
@@ -19,3 +19,103 @@
  * @license http://www.gnu.org/licenses/gpl.txt GNU General Public License 3.0
  */
 
+    namespace Sycamore\Scheduler;
+    
+    use Sycamore\Scheduler\Task\TaskInterface;
+    use Sycamore\Stdlib\ArrayUtils\ArrayLikeValidation;
+    use Sycamore\OS\FileSystem;
+    use Sycamore\OS\Shell;
+    
+    class Scheduler
+    {
+        public static function addTasks(& $tasks)
+        {
+            // Ensure tasks are in array-like form.
+            try {
+                $validTasks = ArrayLikeValidation::validateData($task, get_class($this), true);
+            } catch (\InvalidArgumentException $ex) {
+                throw $ex;
+            }
+            
+            // Ensure individual tasks are of valid type.
+            foreach ($validTasks as $validTask) {
+                if (!$validTask instanceof TaskInterface) {
+                    throw new \InvalidArgumentException("A task was not an instance of a TaskInterface");
+                }
+            }
+            
+            // Add tasks given all are valid.
+            foreach ($validTasks as $validTask) {
+                static::addTask($validTask);
+            }
+            
+            // Return true on success.
+            return true;
+        }
+        
+        /**
+         * Adds the provided task to the OS schedule.
+         * 
+         * @param \Sycamore\Scheduler\Task\TaskInterface $task
+         * 
+         * @return bool
+         */
+        public static function addTask(TaskInterface& $task)
+        {
+            // Ensure GB-styled dates and times work as expected.
+            setlocale(LC_TIME, "en_GB");
+            // Add task as per the type of task.
+            if (OS == WINDOWS || $task->getScheduleType() == TaskInterface::SCHEDULE_ONCE) {
+                $result = Shell::execute($task->getTask());
+            } else {
+                static::addCronTask($task);
+            }
+            
+            // Return true on success.
+            return true;
+        }
+        
+        /**
+         * Adds the provided cron task to the crontab.
+         * 
+         * @param TaskInterface $task
+         * 
+         * @return bool
+         */
+        protected static function addCronTask(TaskInterface& $task)
+        {
+            // Get task string and ID.
+            $taskStr = $task->getTask();
+            $id = $task->getId();
+            
+            // Construct filepath for crontab.
+            $filepath = static::getCrontabFilepath($id);
+            
+            // Put task into file.
+            FileSystem::filePutContents($filepath, $taskStr);
+            
+            // Apply file as crontab.
+            $applyCronTaskCmd = "crontab $filepath";
+            Shell::execute($applyCronTaskCmd);
+            
+            // Delete file.
+            unlink($filepath);
+            
+            // Return true on success.
+            return true;
+        }
+        
+        // TODO(Matthew): Add removeTask and removeCronTask functions.
+        
+        /**
+         * Constructs and returns the crontab filepath for the given crontab ID.
+         * 
+         * @param string $id
+         * 
+         * @return string
+         */
+        protected static function getCrontabFilepath($id)
+        {
+            return TEMP_DIRECTORY . "/crontab/" . $id . ".txt";
+        }
+    }

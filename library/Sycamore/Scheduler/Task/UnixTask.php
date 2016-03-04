96160d5fc758e86264e32b181be75a8d2bff8329
SamThePsychoticLeprechaun <matthew.marshall96@yahoo.co.uk>
2016-03-04 00:13:50 +0000

    Move task construction out to separate functions.

--- library/Sycamore/Scheduler/Task/UnixTask.php
+++ library/Sycamore/Scheduler/Task/UnixTask.php
@@ -46,65 +46,10 @@
             $st = $this->getScheduleType();
             
             // Differentiate between execute once and execute multiple times (at vs crontab).
-            $task = "";
             if ($st == self::SCHEDULE_ONCE) {
-                $task .= "at ";
-                
-                // Add start time.
-                $time = $this->getExecutiveTime();
-                $task .= (isset($time["hour"]) ? $time["hour"] : "00") . ":" . (isset($time["minutes"]) ? $time["minutes"] : "00");
-                
-                // If a date for execution is specified, add it.
-                if ($this->hasExecutiveDate()) {
-                    $date = $this->getExecutiveDate();
-                    $task .= " " . (isset($date["day"]) ? $date["day"] : date("d")) . "." . (isset($date["month"]) ? $date["month"] : date("m")) . "." . (isset($date["year"]) ? $date["year"] : date("Y"));
-                }
-                
-                // Create a temporary shell script for the command (deletes itself on execution).
-                $this->setShellFileHandle();
-                $filehandle = $this->getShellFileHandle();
-                //$filepath = TEMP_DIRECTORY . "/atcmd/" . $filehandle;
-                $filepath = "/tmp/atcmd/" . $filehandle;
-                FileSystem::filePutContents($filepath, $this->getJob() . "\nrm -f $filepath");
-                
-                // Add job to the command line call.
-                $task .= " -f $filepath";
+                $task = $this->constructAtTaskString();
             } else {
-                // Set the ID for this task.
-                $this->set("id", Rand::getString(16, Rand::ALPHANUMERIC));
-                
-                // Add time components to cron task.
-                if ($this->hasExecutiveTime()) {
-                    $time = $this->getExecutiveTime();
-                    $task .= (isset($time["minutes"]) ? $time["minutes"] : "*") . " " . (isset($time["hour"]) ? $time["hour"] : "*") . " ";
-                } else {
-                    $task .= "* * ";
-                }
-                
-                // Add date components to cron task.
-                $hasExecutiveMonths = $this->hasExecutiveMonths();
-                $hasExecutiveDate = $this->hasExecutiveDate();
-                /// Add day and month from date if no executive month specified, otherwise day from date and months from executive months.
-                if (!$hasExecutiveMonths && $hasExecutiveDate) {
-                    $date = $this->getExecutiveDate();
-                    $task .= (isset($date["day"]) ? $date["day"] : "*") . " " . (isset($date["month"]) ? $date["month"] : "*") . " ";
-                } else if ($hasExecutiveMonths) {
-                    if ($hasExecutiveDate) {
-                        $date = $this->getExecutiveDate();
-                        $task .= (isset($date["day"]) ? $date["day"] : "*") . " ";
-                    } else {
-                        $task .= "* ";
-                    }
-                    $task .= $this->getExecutiveMonths() . " ";
-                }
-                /// Add executive days if provided.
-                if ($this->hasExecutiveDays()) {
-                    $task .= $this->getExecutiveDays() . " ";
-                } else {
-                    $task .= "* ";
-                }
-                
-                $task .= $this->getJob();
+                $task = $this->constructAtCronTaskString();
             }
             
             // Set the modified state to false.
@@ -112,6 +57,71 @@
             
             // Set the task string to the newly built task.
             $this->task = $task;
+        }
+        
+        protected function constructAtTaskString()
+        {
+            $task = "at ";
+
+            // Add start time.
+            $time = $this->getExecutiveTime();
+            $task .= (isset($time["hour"]) ? $time["hour"] : "00") . ":" . (isset($time["minutes"]) ? $time["minutes"] : "00");
+
+            // If a date for execution is specified, add it.
+            if ($this->hasExecutiveDate()) {
+                $date = $this->getExecutiveDate();
+                $task .= " " . (isset($date["day"]) ? $date["day"] : date("d")) . "." . (isset($date["month"]) ? $date["month"] : date("m")) . "." . (isset($date["year"]) ? $date["year"] : date("Y"));
+            }
+
+            // Create a temporary shell script for the command (deletes itself on execution).
+            $this->setShellFileHandle();
+            $filehandle = $this->getShellFileHandle();
+            //$filepath = TEMP_DIRECTORY . "/atcmd/" . $filehandle;
+            $filepath = "/tmp/atcmd/" . $filehandle;
+            FileSystem::filePutContents($filepath, $this->getJob() . "\nrm -f $filepath");
+
+            // Add job to the command line call.
+            $task .= " -f $filepath";
+        }
+        
+        protected function constructAtCronTaskString()
+        {
+            // Set the ID for this task.
+            $this->set("id", Rand::getString(16, Rand::ALPHANUMERIC));
+
+            // Add time components to cron task.
+            if ($this->hasExecutiveTime()) {
+                $time = $this->getExecutiveTime();
+                $task .= (isset($time["minutes"]) ? $time["minutes"] : "*") . " " . (isset($time["hour"]) ? $time["hour"] : "*") . " ";
+            } else {
+                $task .= "* * ";
+            }
+
+            // Add date components to cron task.
+            $hasExecutiveMonths = $this->hasExecutiveMonths();
+            $hasExecutiveDate = $this->hasExecutiveDate();
+            /// Add day and month from date if no executive month specified, otherwise day from date and months from executive months.
+            if (!$hasExecutiveMonths && $hasExecutiveDate) {
+                $date = $this->getExecutiveDate();
+                $task .= (isset($date["day"]) ? $date["day"] : "*") . " " . (isset($date["month"]) ? $date["month"] : "*") . " ";
+            } else if ($hasExecutiveMonths) {
+                if ($hasExecutiveDate) {
+                    $date = $this->getExecutiveDate();
+                    $task .= (isset($date["day"]) ? $date["day"] : "*") . " ";
+                } else {
+                    $task .= "* ";
+                }
+                $task .= $this->getExecutiveMonths() . " ";
+            }
+            /// Add executive days if provided.
+            if ($this->hasExecutiveDays()) {
+                $task .= $this->getExecutiveDays() . " ";
+            } else {
+                $task .= "* ";
+            }
+
+            // Add job.
+            $task .= $this->getJob();
         }
         
         /**

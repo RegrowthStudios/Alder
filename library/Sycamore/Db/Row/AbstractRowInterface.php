a8970152501f93efe217820b483ae148c82837da
SamThePsychoticLeprechaun <matthew.marshall96@yahoo.co.uk>
2016-03-10 23:06:52 +0000

    Create abstract row interface and implementation.

---
+++ library/Sycamore/Db/Row/AbstractRowInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright (C) 2016 Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license http://www.gnu.org/licenses/gpl.txt GNU General Public License 3.0
+ */
+
+    namespace Sycamore\Db\Row;
+    
+    interface AbstractRowInterface
+    {
+        /**
+         * Enters the data provided into the row instance.
+         * 
+         * @param array|\Traversable $data The data to be exchanged into the row instance.
+         * 
+         * @throws \InvalidArgumentException if data provided is not an array.
+         */
+        public function exchangeArray($data);
+        
+        /**
+         * Returns the data in this row instance in array form.
+         * 
+         * @return array The data in this row instance.
+         */
+        public function toArray();
+    }
+    

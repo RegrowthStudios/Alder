ee03f8235ce141b75187af39609f4a05728b0dc2
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-03-10 18:28:26 +0000

    Use a set of flags for unique ID length."

--- library/Sycamore/Stdlib/UniqueID.php
+++ library/Sycamore/Stdlib/UniqueID.php
@@ -29,9 +29,13 @@
      */
     class UniqueID
     {
+        const LONG = 1;
+        const MEDIUM = 2;
+        const SHORT = 4;
+        
         /**
          * Generates a partially random, unique ID. If strong is true, then a cryptographically secure random generator is used for a part of the ID.
-         * With moreEntropy set to false, the ID will be 16 characters long, if moreEntropy is true then it will be 32 characters long. (Plus the length of the given prefix).
+         * With the SHORT flag, the ID will be 16 characters long, the MEDIUM flag yields a 19 character ID and the LONG flag a 32 character ID (Plus the length of the given prefix).
          * 
          * @param string $prefix The prefix to attach to the unique ID.
          * @param bool $strong Determines if a cryptographically secure random generator should be used for part of the ID.
@@ -39,8 +43,18 @@
          * 
          * @return string The resulting unique ID.
          */
-        public static function generate($prefix = "", $strong = false, $moreEntropy = false)
+        public static function generate($prefix = "", $strong = false, $flags = self::MEDIUM)
         {
-            return uniqid($prefix . Rand::getString(($moreEntropy ? 9 : 3), Rand::ALPHANUMERIC, $strong), $moreEntropy);
+            $randLength = 6;
+            $moreEntropy = false;
+            if (self::LONG & $flags) {
+                $randLength = 9;
+                $moreEntropy = true;
+            } else if (self::SHORT & $flags) {
+                $randLength = 3;
+            } else if (!(self::MEDIUM & $flags)) {
+                throw new \InvalidArgumentException("The flags provided were invalid!");
+            }
+            return uniqid($prefix . Rand::getString($randLength, Rand::ALPHANUMERIC, $strong), $moreEntropy);
         }
     }

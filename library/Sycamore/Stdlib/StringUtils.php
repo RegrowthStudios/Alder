f7232984a4a0a419d11fd7cb53046b312b63a52e
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-03-16 21:29:11 +0000

    Add a function for determining if strings end with a given substring.

--- library/Sycamore/Stdlib/StringUtils.php
+++ library/Sycamore/Stdlib/StringUtils.php
@@ -67,5 +67,22 @@
             }
             return str_replace(array("\\", "/"), "_", $string);
         }
+        
+        /**
+         * Determines if one string ends with another string.
+         * 
+         * @param string $haystack The string to evaluate the ending of.
+         * @param string $needle The string to search for at the end of $haystack.
+         * @return boolean True if $haystack ends with $needle, false otherwise.
+         */
+        public static function endsWith($haystack, $needle)
+        {
+            $haystackLength = strlen($haystack);
+            $needleLength = strlen($needle);
+            if ($haystackLength < $needleLength) {
+                return false;
+            }
+            return substr_compare($haystack, $needle, $haystackLength - $needleLength, $needleLength) === 0;
+        }
     }
         

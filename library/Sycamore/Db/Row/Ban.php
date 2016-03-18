c5e6575b87f2e15ef07fd2b2f0d625c649ab8027
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-03-18 17:22:02 +0000

    Fix namespacing.

--- library/Sycamore/Db/Row/Ban.php
+++ library/Sycamore/Db/Row/Ban.php
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-    namespace Sycamore\Row;
+    namespace Sycamore\Db\Row;
     
     use Sycamore\Db\Row\AbstractObjectRow;
     

5b58e6fc337a3709e713fc8aa4e60d028c1e0656
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-03-19 01:21:47 +0000

    Implement visitor class for visiting user information.

--- library/Sycamore/Visitor.php
+++ library/Sycamore/Visitor.php
@@ -19,3 +19,79 @@
  * @license http://www.gnu.org/licenses/gpl.txt GNU General Public License 3.0
  */
 
+    namespace Sycamore;
+    
+    use Zend\ServiceManager\ServiceLocatorInterface;
+    
+    class Visitor
+    {
+        /**
+         * Stores data pertaining to the visitor.
+         *
+         * @var array 
+         */
+        protected $data;
+        
+        /**
+         * Grabs any existing session data if visitor is logged in and stores it.
+         * 
+         * @param ServiceLocatorInterface $serviceManager The service manager for this application instance.
+         * 
+         * @return \Sycamore\Visitor Self
+         */
+        public function __construct(ServiceLocatorInterface& $serviceManager)
+        {
+            // Grab the user session helper.
+            $userSession = $serviceManager->get("Sycamore\User\Session");
+            
+            // Grab token payload if SLIS exists.
+            $tokenPayload = [];
+            if (!$userSession->acquire($tokenPayload)) {
+                $this->data["isLoggedIn"] = false;
+                return;
+            }
+            
+            // Visitor is logged in, add data here.
+            $this->data = array_merge($tokenPayload["applicationPayload"], [
+                "isLoggedIn" => true
+            ]);
+        }
+        
+        /**
+         * Determines if the visitor is logged in.
+         * 
+         * @return bool True if logged in, false otherwise.
+         */
+        public function isLoggedIn()
+        {
+            return $this->data["isLoggedIn"];
+        }
+        
+        /**
+         * Gets the specified property of the visitor. E.g. "id", "superUser" etc..
+         * 
+         * @param string $property The property to get.
+         * 
+         * @return mixed The value of the given property if it exists.
+         * 
+         * @throws \InvalidArgumentException if $property is not a valid property key.
+         */
+        public function get($property)
+        {
+            if (isset($this->data[$property])) {
+                return $this->data[$property];
+            }
+            
+            // TODO(Matthew): Add functions to fetch some common properties of the visiting user, for performance purposes?
+            $fetchFunc = "fetch" . ucfirst($property);
+            if (method_exists($this, $fetchFunc)) {
+                $result = $this->{$fetchFunc}();
+                if (!is_null($result)) {
+                    $this->data[$property] = $result;
+                    return $result;
+                }
+            }
+            
+            throw new \InvalidArgumentException("The property, $property, is not valid for Visitor.");
+        }
+    }

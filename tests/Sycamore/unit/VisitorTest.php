16eb13998bbdc5b6077ccd6a36dfc494ee4d710b
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-03-22 23:30:20 +0000

    Begin writing tests for Visitor class.

---
+++ tests/Sycamore/unit/VisitorTest.php
@@ -0,0 +1,22 @@
+<?php
+    namespace SycamoreTest\Sycamore;
+    
+    /**
+     * Test functionality of Sycamore's Visitor class.
+     *
+     * @author Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @since 0.1.0
+     */
+    class VisitorTest extends \PHPUnit_Framework_TestCase
+    {
+        /**
+         * {@inheritdoc}
+         */
+        protected function setUp()
+        {
+            // Hmm...
+        }
+        
+        public
+    }

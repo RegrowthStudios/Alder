e592f9c7a49c453f16f579828ed23fbb092652a3
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-03-26 11:42:07 +0000

    Implement test for onBootstrap function and service creators.

---
+++ tests/Sycamore/unit/ModuleTest.php
@@ -0,0 +1,82 @@
+<?php
+    namespace SycamoreTest\Sycamore;
+
+    use Sycamore\Module;
+    
+    use Zend\EventManager\EventManager;
+    use Zend\Mvc\Application;
+    use Zend\Mvc\MvcEvent;
+    use Zend\ServiceManager\ServiceManager;
+    
+    /**
+     * Test functionality of Sycamore's Module class.
+     *
+     * @author Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @since 0.1.0
+     */
+    class ModuleTest extends \PHPUnit_Framework_TestCase
+    {
+        /**
+         * @test
+         * 
+         * @covers \Sycamore\Module::onBootstrap
+         * @covers \Sycamore\Module::prepareServices
+         * @covers \Sycamore\Module::createDatabaseCacheService
+         * @covers \Sycamore\Module::createSycamoreTableCacheService
+         */
+        public function bootstrapConstructsServicesTest()
+        {            
+            $eventManager = $this->getMockBuilder(EventManager::class)
+                    ->setMethods(["attach"])
+                    ->getMock();
+            
+            $serviceManager = $this->getMockBuilder(ServiceManager::class)
+                    ->setMethods(["get", "setService"])
+                    ->getMock();
+            $serviceManager->method("get")
+                    ->willReturn([ "Sycamore" => [ "cache" => [
+                        "namespace" => "sycamore_cache",
+                        "timeToLive" => 1800,
+                        "adapter" => "filesystem",
+                        "plugins" => [
+                            "clearExpired" => [
+                                "clearingFactor" => 100,
+                            ],
+                            "ignoreUserAbort" =>  [
+                                "exitOnAbort" => false,
+                            ],
+                            "optimise" => [
+                                "optimisingFactor" => 100,
+                            ]
+                        ]
+                    ]]]);
+            $serviceManager->expects($this->exactly(2))
+                    ->method("setService")
+                    ->withConsecutive(
+                            ["SycamoreDbCache"],
+                            ["SycamoreTableCache"]
+                    );
+            
+            $application = $this->getMockBuilder(Application::class)
+                    ->disableOriginalConstructor()
+                    ->setMethods(["getEventManager", "getServiceManager"])
+                    ->getMock();
+            $application->method("getEventManager")
+                    ->willReturn($eventManager);
+            $application->method("getServiceManager")
+                    ->willReturn($serviceManager);
+            
+            $event = $this->getMockBuilder(MvcEvent::class)
+                    ->setMethods(["getApplication"])
+                    ->getMock();
+            $event->method("getApplication")
+                    ->willReturn($application);
+            
+            $module = $this->getMockBuilder(Module::class)
+                    ->disableArgumentCloning()
+                    ->setMethods(NULL)
+                    ->getMock();
+            $module->onBootstrap($event);
+        }
+    }

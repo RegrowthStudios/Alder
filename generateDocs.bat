8a06457c8ac7227209230a48eeba145fe7f48aa3
SamThePsychoticLeprechaun <matthew.marshall96@yahoo.co.uk>
2016-03-27 14:45:38 +0100

    Generate docs for all modules.

--- generateDocs.bat
+++ generateDocs.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-IF EXIST modules\Sycamore ( GOTO DIR_EXISTS )
+IF EXIST modules ( GOTO DIR_EXISTS )
 
 ECHO No path was found for the modules of this Sycamore project to generate docs from.
 PAUSE
@@ -8,4 +8,4 @@ EXIT 0
 
 :DIR_EXISTS
 ECHO Generating docs...
-PHPDOC run -d "%~dp0modules\Sycamore" -t "%~dp0docs" --title="Sycamore" --sourcecode --template="responsive-twig"
+PHPDOC run -d "%~dp0modules" -t "%~dp0docs" --title="Sycamore" --sourcecode --template="responsive-twig"

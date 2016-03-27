52537be10f0c2b48a6c67be4f9e5e6fb1be45e07
SamThePsychoticLeprechaun <matthew.marshall96@yahoo.co.uk>
2016-03-27 16:29:01 +0100

    Add tests for ConfigUtils class.

---
+++ tests/Sycamore/unit/Config/ConfigUtilsTest.php
@@ -0,0 +1,72 @@
+<?php
+    namespace SycamoreTest\Sycamore\Config;
+    
+    use Sycamore\Config\ConfigUtils;
+    
+    /**
+     * Test functionality of Sycamore's TableCache class.
+     *
+     * @author Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @since 0.1.0
+     */
+    class ConfigUtilsTest extends \PHPUnit_Framework_TestCase
+    {
+        /**
+         * Remove all temporarily generated files.
+         */
+        protected function tearDown()
+        {
+            $files = glob(TEMP_DIRECTORY . "/*");
+            foreach ($files as $file) {
+                if (is_file($file)) {
+                    unlink($file);
+                }
+            }
+        }
+        
+        /**
+         * @test
+         * 
+         * @covers \Sycamore\Cache\TableCacheTest::save
+         */
+        public function configFileGeneratesCorrectlyTest()
+        {
+            $config = [
+                "test"
+            ];
+            
+            $filepath = TEMP_DIRECTORY . "/conf.php";
+            
+            ConfigUtils::save($filepath, $config);
+            
+            $result = require $filepath;
+            
+            $this->assertEquals($config, $result);
+        }
+        
+        /**
+         * @test
+         * 
+         * @covers \Sycamore\Config\ConfigUtils::save
+         */
+        public function configFileOverwritesCorrectlyTest()
+        {
+            $config = [
+                "test"
+            ];
+            $config2 = [
+                "test2"
+            ];
+            
+            $filepath = TEMP_DIRECTORY . "/conf.overwrite.php";
+            
+            ConfigUtils::save($filepath, $config);
+            ConfigUtils::save($filepath, $config2);
+            
+            $result = require $filepath;
+            
+            $this->assertEquals($config2, $result);
+        }
+    }
+    

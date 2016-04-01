d273c893f2d2ec44b18dfee2532c4f86b70e8600
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-04-01 16:18:22 +0100

    Include the constants file.

--- tests/index.php
+++ tests/index.php
@@ -10,43 +10,8 @@
 
     error_reporting(E_ALL | E_STRICT);
     chdir(__DIR__);
-
-    // Define a bunch of directory constants.
-    define("APP_DIRECTORY", dirname(__DIR__));
-    define("MODULES_DIRECTORY", APP_DIRECTORY.DIRECTORY_SEPARATOR."modules");
-    define("VENDOR_DIRECTORY", APP_DIRECTORY.DIRECTORY_SEPARATOR."vendor");
-    define("CONFIG_DIRECTORY", APP_DIRECTORY.DIRECTORY_SEPARATOR."config");
-    define("LOGS_DIRECTORY", APP_DIRECTORY.DIRECTORY_SEPARATOR."logs");
-    define("TEMP_DIRECTORY", APP_DIRECTORY.DIRECTORY_SEPARATOR."temp");
-    define("CACHE_DIRECTORY", APP_DIRECTORY.DIRECTORY_SEPARATOR."cache");
-    define("SYCAMORE_MODULE_DIRECTORY", MODULES_DIRECTORY.DIRECTORY_SEPARATOR."Sycamore");
-
-    // Define possible ENV states.
-    define("PRODUCTION", "production");
-    define("TESTING", "testing");
-    define("DEVELOPMENT", "development");
-
-    // Define ENV - the environment state (development or production).
-    // ENV state stored in a PHP file to get around Nginx not supporting setting environment variables a la Apache.
-    define("ENV", require CONFIG_DIRECTORY."/env.state.php");
-
-    // Define value to set config values to that MUST be overridden by a given installation.
-    define("DEFAULT_VAL", "CHANGE");
-
-    // Define appropriate OS constant.
-    define("UNIX", "Unix");
-    define("WINDOWS", "Windows");
-    switch (php_uname("s")) {
-        // Treat FreeBSD and Linux as basically UNIX because they'll be treated the same as UNIX OS's anyway.
-        case "FreeBSD":
-        case "Linux":
-        case "Solaris":
-            define("OS", UNIX);
-            break;
-        case "Windows NT":
-            define("OS", WINDOWS);
-            break;
-    }
+    
+    require dirname(__DIR__) . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "sycamore.constants.php";
     
     require "./Bootstrap.php";
     require "./TestHelpers.php";

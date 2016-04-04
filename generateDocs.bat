10f1c27bc02848d236664c4018de9fab5a52536c
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-04-04 21:23:50 +0100

    Add check for PHPDoc utility.

--- generateDocs.bat
+++ generateDocs.bat
@@ -1,11 +1,20 @@
 @ECHO OFF
 
-IF EXIST modules ( GOTO DIR_EXISTS )
+IF NOT EXIST modules ( GOTO NO_DIR_EXISTS )
 
-ECHO No path was found for the modules of this Sycamore project to generate docs from.
-PAUSE
-EXIT 0
+WHERE PHPDOC >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 ( GOTO NO_PHPDOC )
 
-:DIR_EXISTS
 ECHO Generating docs...
 PHPDOC run -d "%~dp0modules" -t "%~dp0docs" --title="Sycamore" --sourcecode --template="responsive-twig"
+EXIT /B 0
+
+:NO_DIR_EXISTS
+ECHO No path was found for the modules of this Sycamore project to generate docs from.
+PAUSE
+EXIT /B 1
+
+:NO_PHPDOC
+ECHO No PHPDOC utility could be found, please add it to your environment path!
+PAUSE
+EXIT /B 2

decde96d3db39b136eaebb2920065a36923c0116
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-04-05 02:56:08 +0100

    Add comment with phpunit call.

--- runTests.bat
+++ runTests.bat
@@ -1,3 +1,5 @@
 @ECHO OFF
 
+REM phpunit --log-json=test_results/test_results.json --coverage-html=test_results/coverage/ --coverage-php=test_results/coverage.php --testdox-html=test_results/test_results.html
+
 EXIT /B 0

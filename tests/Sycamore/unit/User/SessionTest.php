0be1bfed5330e699888b85199ce215a918362f3d
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-04-22 17:00:33 +0100

    Add extra tests for the user session class.

--- tests/Sycamore/unit/User/SessionTest.php
+++ tests/Sycamore/unit/User/SessionTest.php
@@ -38,6 +38,8 @@ namespace SycamoreTest\Sycamore\User {
     use Sycamore\Token\Jwt;
     use Sycamore\User\Session;
     
+    use SycamoreTest\Bootstrap;
+    
     use Zend\ServiceManager\ServiceManager;
     
     /**
@@ -117,6 +119,17 @@ namespace SycamoreTest\Sycamore\User {
         /**
          * @test
          * 
+         * @covers \Sycamore\User\Session::__construct
+         */
+        public function sessionClassCorrectlyConstructsTest()
+        {
+            $sessionManager = new Session(Bootstrap::getServiceManager());
+            $this->assertTrue($sessionManager instanceof Session);
+        }
+        
+        /**
+         * @test
+         * 
          * @covers \Sycamore\User\Session::create
          */
         public function sessionCreationSetsCookieCorrectlyTest()
@@ -127,11 +140,108 @@ namespace SycamoreTest\Sycamore\User {
         /**
          * @test
          * 
+         * @covers \Sycamore\User\Session::create
+         */
+        public function sessionCreationReturnsFalseIfInvalidUserTest()
+        {
+            $config = [
+                "Sycamore" => [
+                    "domain" => "example.com",
+                    "security" => [
+                        "sessionLength" => 36400,
+                        "sessionLengthExtended" => 364000,
+                        "sessionsOverHttpsOnly" => false,
+                        "accessCookiesViaHttpOnly" => false,
+                        "tokenPrivateKey" => "test",
+                        "tokenPublicKey" => "test",
+                        "tokenHashAlgorithm" => "HS256",
+                        "verifyTokenLifetime" => 21600
+                    ],
+                ]
+            ];
+            
+            $table = $this->getMockBuilder(User::class)
+                    ->disableOriginalConstructor()
+                    ->setMethods(["getByEmail", "getByUsername"])
+                    ->getMock();
+            $table->method("getByEmail")
+                    ->willReturn(false);
+            $table->method("getByUsername")
+                    ->willReturn(false);
+            
+            $tableCache = $this->getMockBuilder(TableCache::class)
+                    ->disableOriginalConstructor()
+                    ->setMethods(["fetchTable"])
+                    ->getMock();
+            $tableCache->method("fetchTable")
+                    ->willReturn($table);
+            
+            $serviceManager = $this->getMockBuilder(ServiceManager::class)
+                    ->disableOriginalConstructor()
+                    ->setMethods(["get"])
+                    ->getMock();
+            $serviceManager->method("get")
+                    ->will($this->returnCallback(function($key) use ($tableCache, $config) {
+                        if ($key == "SycamoreTableCache") {
+                            return $tableCache;
+                        } else if ($key == "Config") {
+                            return $config;
+                        } else {
+                            return NULL;
+                        }
+                    }));
+            
+            $sessionManager = $this->getMockBuilder(Session::class)
+                    ->setConstructorArgs([&$serviceManager])
+                    ->setMethods(NULL)
+                    ->getMock();
+            
+            $this->assertFalse($sessionManager->create("test"));
+        }
+        
+        /**
+         * @test
+         * 
+         * @covers \Sycamore\User\Session::create
+         */
+        public function sessionCreationSetsCookieCorrectlyForExtendedSessionTest()
+        {            
+            $this->assertTrue(is_string($this->sessionManager->create("test", true)));
+        }
+        
+        /**
+         * @test
+         * 
+         * @covers \Sycamore\User\Session::create
          * @covers \Sycamore\User\Session::acquire
          */
-        public function sessionAcquireReturnsTrueAndProvidesAppClaimsForValidSessionTest()
+        public function sessionAcquireReturnsTrueAndProvidesAppClaimsForValidEmailedSessionTest()
         {
             $token = $this->sessionManager->create("test", false);
+            
+            $_COOKIE["SLIS"] = $token;
+            
+            $tokenClaims = [];
+            $result = $this->sessionManager->acquire($tokenClaims);
+            
+            $this->assertTrue($result === Jwt::VALID);
+            $this->assertTrue(
+                $tokenClaims->id === 1 &&
+                $tokenClaims->username === "test" &&
+                $tokenClaims->email === "test@example.com" &&
+                $tokenClaims->superuser === false
+            );
+        }
+        
+        /**
+         * @test
+         * 
+         * @covers \Sycamore\User\Session::create
+         * @covers \Sycamore\User\Session::acquire
+         */
+        public function sessionAcquireReturnsTrueAdProvidesAppClaimsForValidUsernamedSessionTest()
+        {
+            $token = $this->sessionManager->create("testm9@yahoo.com", false);
             
             $_COOKIE["SLIS"] = $token;
             
@@ -164,6 +274,32 @@ namespace SycamoreTest\Sycamore\User {
             
             $this->assertFalse($result === Jwt::VALID);
             $this->assertEmpty($tokenClaims);
+        }
+        
+        /**
+         * @test
+         * 
+         * @covers \Sycamore\User\Session::acquire
+         */
+        public function sessionAcquireReturnsZeroForUnsetCookieTest()
+        {
+            unset($_COOKIE["SLIS"]);
+            
+            $tokenClaims = [];
+            $this->assertSame(0, $this->sessionManager->acquire($tokenClaims));
+        }
+        
+        /**
+         * @test
+         * 
+         * @covers \Sycamore\User\Session::acquire
+         */
+        public function sessionAcquireReturnsZeroForFalsyCookieTest()
+        {
+            $_COOKIE["SLIS"] = false;
+            
+            $tokenClaims = [];
+            $this->assertSame(0, $this->sessionManager->acquire($tokenClaims));
         }
     }
 }

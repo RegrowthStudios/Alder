33f7fd2a091390a9203f762b7953b64d121d4eec
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-06-26 04:01:40 +0100

    Removed archived folder.

--- alder_public_authentication - Copy/runTests.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-phpunit --log-json=test_results/test_results.json --coverage-html=test_results/coverage/ --coverage-php=test_results/coverage.php --testdox-html=test_results/test_results.html
-
-EXIT /B 0

cccfa249ac543ede6e729787f17641d14364e638
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-08-08 15:33:46 +0100

    Add file storing application mode.

---
+++ alder_public_authentication/config/env.state.php
@@ -0,0 +1,9 @@
+<?php
+
+    /**
+     * Return the environment state to run Alder in.
+     * Possible options are:
+     *  - DEVELOPMENT -- Debugging and timing are fully enabled.
+     *  - PRODUCTION -- No debugging or timing.
+     */
+    return DEVELOPMENT;

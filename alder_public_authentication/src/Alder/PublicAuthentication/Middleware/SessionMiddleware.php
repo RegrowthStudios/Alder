861ba3ced4a84dc69faf04586cacfe905b372cbc
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-08-10 20:38:18 +0100

    Some work towards completing the session middleware.

--- alder_public_authentication/src/Alder/PublicAuthentication/Middleware/SessionMiddleware.php
+++ alder_public_authentication/src/Alder/PublicAuthentication/Middleware/SessionMiddleware.php
@@ -2,6 +2,13 @@
 
     namespace Alder\PublicAuthentication\Middleware;
     
+    use Lcobucci\JWT\Builder;
+    
+    use Psr\Http\Message\ResponseInterface;
+    use Psr\Http\Message\ServerRequestInterface;
+    
+    use Zend\Stratigility\MiddlewareInterface;
+    
     /**
      * The session middleware for Alder's public authentication service.
      * Determines if a visitor already has a session JWT and if so packs the data into 
@@ -11,7 +18,12 @@
      * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
      * @since 0.1.0
      */
-    class SessionMiddleware
+    class SessionMiddleware implements MiddlewareInterface
     {
-        
+        public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $out = null)
+        {
+            $alis = $request->getCookieParams()["alis"];
+            
+            
+        }
     }

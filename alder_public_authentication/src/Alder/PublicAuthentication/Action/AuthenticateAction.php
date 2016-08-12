ffe0bcbcaf7af2a65780bba99aa8bf08d4cf7a07
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-08-12 19:31:48 +0100

    Create action for user authentication.

---
+++ alder_public_authentication/src/Alder/PublicAuthentication/Action/AuthenticateAction.php
@@ -0,0 +1,18 @@
+<?php
+
+    namespace Alder\PublicAuthentication\Action;
+    
+    use Alder\PublicAuthentication\Action\AbstractRestfulAction;
+    
+    /**
+     * The user action middleware for Alder's public authentication service.
+     * Handles user-entity actions based on request and session information.
+     * 
+     * @author Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
+     * @since 0.1.0
+     */
+    abstract class AuthenticateAction extends AbstractRestfulAction
+    {
+        
+    }

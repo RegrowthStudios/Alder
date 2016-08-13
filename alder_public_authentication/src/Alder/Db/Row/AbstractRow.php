d3db88f29bc2dc65f1d435ab155fe0f791e6c159
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-08-13 02:35:38 +0100

    Add abstract row class and interface.

---
+++ alder_public_authentication/src/Alder/Db/Row/AbstractRow.php
@@ -0,0 +1,39 @@
+<?php
+    namespace Sycamore\Db\Row;
+    
+    use Sycamore\Db\Row\AbstractRowInterface;
+    use Sycamore\Stdlib\ArrayUtils;
+    
+    /**
+     * Abstract row representation class, implementing functions for creating rows from, and transforming rows into, arrays.
+     * 
+     * @author Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
+     * @since 0.1.0
+     * @abstract
+     */
+    abstract class AbstractRow implements AbstractRowInterface
+    {
+        /**
+         * {@inheritdoc}
+         */
+        public function exchangeArray($data)
+        {
+            $validatedData = ArrayUtils::validateArrayLike($data, get_class($this), true);
+            $oldData = get_object_vars($this);
+            foreach($oldData as $key => $_) {
+                if (isset($validatedData[$key])) {
+                    $this->$key = $validatedData[$key];
+                }
+            }
+            return $oldData;
+        }
+        
+        /**
+         * {@inheritdoc}
+         */
+        public function toArray()
+        {
+            return get_object_vars($this);
+        }
+    }

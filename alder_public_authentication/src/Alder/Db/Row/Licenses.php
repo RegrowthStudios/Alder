d590d49d3c3746d773b805c9c821c2d28a3b52d3
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-08-15 00:51:49 +0100

    Add database row representation.

---
+++ alder_public_authentication/src/Alder/Db/Row/Licenses.php
@@ -0,0 +1,19 @@
+<?php
+
+    namespace Alder\Db\Row;
+
+    use Alder\Db\Row\AbstractRow;
+
+    /**
+     * Representation of a row in the table of licenses.
+     *
+     * @author Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
+     * @since 0.1.0
+     */
+    class Licenses extends AbstractRow
+    {
+        public $id;
+        public $product_id;
+        public $simultaneous_usage_count;
+    }

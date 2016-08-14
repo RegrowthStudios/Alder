b8ed87d33e6b962503b23aab44a892305655e57a
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-08-15 00:49:10 +0100

    Create function for establishing user log-in sessions.

--- alder_public_authentication/src/Alder/PublicAuthentication/Action/AuthenticateAction.php
+++ alder_public_authentication/src/Alder/PublicAuthentication/Action/AuthenticateAction.php
@@ -3,6 +3,9 @@
     namespace Alder\PublicAuthentication\Action;
     
     use Alder\PublicAuthentication\Action\AbstractRestfulAction;
+    use Alder\Stdlib\ActionUtils;
+
+    use Zend\Diactoros\Response\JsonResponse;
     
     /**
      * The user action middleware for Alder's public authentication service.
@@ -14,5 +17,25 @@
      */
     abstract class AuthenticateAction extends AbstractRestfulAction
     {
-        
+        protected function create() {
+            $password = $this->getParameter("password");
+            $username = $this->getParameter("username");
+            $email    = $this->getParameter("email");
+
+            $errors = [];
+            if (!$password) {
+                $errors[] = 1010101;
+            }
+            if (!$username && !$email) {
+                $errors[] = 1010102;
+            }
+            if (!empty($errors)) {
+                $this->response = new JsonResponse([
+                    "errors" => $errors
+                ], 400);
+                return;
+            }
+
+
+        }
     }

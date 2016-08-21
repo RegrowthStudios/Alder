e8d0581b3abf746aea00daac5a45f45f5c987dfc
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-08-21 18:56:49 +0100

    Move app-specific rows to correct dir.

---
+++ alder_public_authentication/src/Alder/PublicAuthentication/Db/Row/User.php
@@ -0,0 +1,22 @@
+<?php
+
+    namespace Alder\Db\Row;
+
+    use Alder\Db\Row\AbstractRow;
+
+    /**
+     * Representation of a row in the table of users.
+     *
+     * @author Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
+     * @since 0.1.0
+     */
+    class User extends AbstractRow
+    {
+        public $id;
+        public $username;
+        public $primary_email_local;
+        public $primary_email_domain;
+        public $password_hash;
+        public $employee_flag;
+    }

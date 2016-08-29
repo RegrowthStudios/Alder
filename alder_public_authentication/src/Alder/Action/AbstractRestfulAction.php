fc901ffd47746ff85d35d990883e32d93639f162
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-08-29 21:42:20 +0100

    Use middleware trait and implement general options action functionality.

--- alder_public_authentication/src/Alder/Action/AbstractRestfulAction.php
+++ alder_public_authentication/src/Alder/Action/AbstractRestfulAction.php
@@ -1,17 +1,19 @@
 <?php
 
     namespace Alder\Action;
-    
+
+    use Alder\Middleware\MiddlewareTrait;
+
     use Psr\Http\Message\ResponseInterface;
     use Psr\Http\Message\ServerRequestInterface;
 
+    use Zend\Diactoros\Response\JsonResponse;
     use Zend\Diactoros\Stream;
     use Zend\Json\Json;
     use Zend\Stratigility\MiddlewareInterface;
 
     /**
-     * The user license action middleware for Alder's public authentication service.
-     * Handles user-license-entity actions based on request and session information.
+     * Provides abstract functionality for actions, such as routing by request method.
      * 
      * @author Matthew Marshall <matthew.marshall96@yahoo.co.uk>
      * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
@@ -20,33 +22,15 @@
      */
     abstract class AbstractRestfulAction implements MiddlewareInterface
     {
-        /**
-         * The request sent by the client.
-         *
-         * @var \Psr\Http\Message\ServerRequestInterface
-         */
-        protected $request;
-        
-        /**
-         * The response to be delivered to the request sender.
-         *
-         * @var \Psr\Http\Message\ResponseInterface 
-         */
-        protected $response;
-        
-        /**
-         * Constant of content types.
-         * 
-         * @var array
-         */
-        const CONTENT_TYPES = [
-            "json" => [
-                "application/hal+json",
-                "application/json",
-                "text/x-json" // Not valid, but legacy software may use this. Better safe than sorry.
-            ]
-        ];
-        
+        use MiddlewareTrait;
+
+//        /**
+//         * The array of data regarding actionable requests for the given URI.
+//         *
+//         * @var array
+//         */
+//        protected $options = [];
+
         // TODO(Matthew): Add more details on how these functions may satisfy the related RFCs.
         /**
          * Processes a request to acquire a specific resource.
@@ -58,15 +42,6 @@
         protected function get($data) {
             $this->response = $this->response->withStatus(405, "Method Not Allowed");
         }
-        
-//        /**
-//         * Processes a request to acquire a set of specific resources.
-//         *
-//         * @abstract
-//         */
-//        protected function getList() {
-//            $this->response = $this->response->withStatus(405, "Method Not Allowed");
-//        }
         
         /**
          * Processes a request to create a new resource.
@@ -90,15 +65,6 @@
             $this->response = $this->response->withStatus(405, "Method Not Allowed");
         }
         
-//        /**
-//         * Processes a request to update a set of resources.
-//         *
-//         * @abstract
-//         */
-//        protected function updateList() {
-//            $this->response = $this->response->withStatus(405, "Method Not Allowed");
-//        }
-        
         /**
          * Processes a request to replace a specific resource.
          *
@@ -109,15 +75,6 @@
         protected function replace($data) {
             $this->response = $this->response->withStatus(405, "Method Not Allowed");
         }
-        
-//        /**
-//         * Processes a request to replace a set of resources.
-//         *
-//         * @abstract
-//         */
-//        protected function replaceList() {
-//            $this->response = $this->response->withStatus(405, "Method Not Allowed");
-//        }
         
         /**
          * Processes a request to delete a specific resource.
@@ -130,22 +87,28 @@
             $this->response = $this->response->withStatus(405, "Method Not Allowed");
         }
         
-//        /**
-//         * Processes a request to delete a set of resources.
-//         *
-//         * @abstract
-//         */
-//        protected function deleteList() {
-//            $this->response = $this->response->withStatus(405, "Method Not Allowed");
-//        }
-        
         /**
          * Processes a request for the options related to the route path of the request.
          * 
          * @abstract
          */
         protected function options() {
-            $this->response = $this->response->withStatus(405, "Method Not Allowed");
+            $action = constant(strtoupper(str_replace("Action", "", end(explode("\\", get_class($this))))));
+
+            if ($action === NULL) {
+                $this->response = $this->response->withStatus(405, "Method Not Allowed");
+                return;
+            }
+
+            $apiMap = require file_build_path(APP_DIRECTORY, "api-map.php");
+
+            if (!isset($apiMap[$action])) {
+                $this->response = $this->response->withStatus(405, "Method Not Allowed");
+                return;
+            }
+
+            $actionMap = $apiMap[$action];
+            $this->response = (new JsonResponse($actionMap["body"]))->withHeader("Allow", $actionMap["allowed"]);
         }
 
         /**
@@ -162,13 +125,13 @@
         /**
          * Determines the appropriate action function to call for the request method and parameters.
          * 
-         * @param ServerRequestInterface $request The request object.
-         * @param ResponseInterface $response The response object.
+         * @param \Psr\Http\Message\ServerRequestInterface $request The request object.
+         * @param \Psr\Http\Message\ResponseInterface $response The response object.
          * @param callable $next The next middleware to be called.
          * 
          * @return NULL|\Psr\Http\Message\ResponseInterface
          */
-        public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
+        public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = NULL)
         {
             $this->request = $request;
             $this->response = $response;
@@ -205,32 +168,5 @@
                 return $next($this->request, $this->response);
             }
             return $this->response;
-        }
-        
-        /**
-         * Fetches the named parameter from the route matching first, and if nothing is found in the route, from
-         * the parsed body if method type is POST, else from the query string. Returns NULL if nothing is found.
-         * 
-         * @param string $parameterHandle The handle of the parameter to be fetched.
-         * @param mixed $default The default value to supply if no parameter value found.
-         * 
-         * @return string The parameter fetched.
-         */
-        protected function getParameter($parameterHandle, $default = NULL) {
-            $param = NULL;
-
-            $params = $this->request->getQueryParams();
-            if (isset($params["$parameterHandle"])) {
-                $param = $params["$parameterHandle"];
-            }
-
-            $parsedBody = $this->request->getParsedBody();
-            if ($parsedBody && isset($parsedBody["$parameterHandle"])) {
-                $param = $parsedBody["$parameterHandle"];
-            }
-            
-            $result = $this->request->getAttribute($parameterHandle, $param);
-
-            return is_null($result) ? $default : $result;
         }
     }

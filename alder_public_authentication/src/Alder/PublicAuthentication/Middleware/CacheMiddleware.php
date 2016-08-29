d5ba0cfa109f4e4c9634ba189f35f4437a6c62f3
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-08-29 21:39:05 +0100

    Give cache middleware shell code.

--- alder_public_authentication/src/Alder/PublicAuthentication/Middleware/CacheMiddleware.php
+++ alder_public_authentication/src/Alder/PublicAuthentication/Middleware/CacheMiddleware.php
@@ -1,7 +1,12 @@
 <?php
 
     namespace Alder\PublicAuthentication\Middleware;
-    
+
+    use Psr\Http\Message\ResponseInterface;
+    use Psr\Http\Message\ServerRequestInterface;
+
+    use Zend\Stratigility\MiddlewareInterface;
+
     /**
      * The cache middleware for Alder's public authentication service.
      * Determines if a request with the same parameters has already been made and cached.
@@ -12,7 +17,18 @@
      * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
      * @since 0.1.0
      */
-    class CacheMiddleware
+    class CacheMiddleware implements MiddlewareInterface
     {
-        
+        /**
+         * Handles caching
+         *
+         * @param \Psr\Http\Message\ServerRequestInterface $request
+         * @param \Psr\Http\Message\ResponseInterface $response
+         * @param callable|NULL $next
+         *
+         * @return \Psr\Http\Message\ResponseInterface The response produced.
+         */
+        public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = NULL) {
+            return $next($request, $response);
+        }
     }

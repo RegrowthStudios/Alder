fab199eb034a13ff5c5d686e083c0e28fd491738
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-09-05 03:58:12 +0100

    Remove specific functions for table operations for now.

--- alder_public_authentication/src/Alder/PublicAuthentication/Db/Table/UserAudit.php
+++ alder_public_authentication/src/Alder/PublicAuthentication/Db/Table/UserAudit.php
@@ -3,27 +3,27 @@
     namespace Alder\PublicAuthentication\Db\Table;
 
     use Alder\Db\Table\AbstractTable;
-    use Alder\PublicAuthentication\Db\Row\User as UserRow;
+    use Alder\PublicAuthentication\Db\Row\UserAudit as UserAuditRow;
 
     /**
-     * Gateway for the user table.
+     * Gateway for the user audit table.
      *
      * @author Matthew Marshall <matthew.marshall96@yahoo.co.uk>
      * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
      * @since 0.1.0
      */
-    class User extends AbstractTable
+    class UserAudit extends AbstractTable
     {
         /**
          * @var string Name of the table.
          */
-        const NAME = "users";
+        const NAME = "user_audits";
 
         /**
          * Prepare the user table gateway, establishing the row prototype.
          */
         public function __construct()
         {
-            parent::__construct(self::NAME, new UserRow());
+            parent::__construct("user_audits", new UserAuditRow());
         }
     }

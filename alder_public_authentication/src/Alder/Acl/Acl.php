2c75e01dc51532ab1a99278f6974a8bed4d334f0
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-09-19 22:54:05 +0100

    Some more work towards ACL system.

--- alder_public_authentication/src/Alder/Acl/Acl.php
+++ alder_public_authentication/src/Alder/Acl/Acl.php
@@ -13,28 +13,66 @@
      */
     class Acl
     {
+        /**
+         * Cache file for the ACL object.
+         */
         const ACL_CACHE = CACHE_DIRECTORY . DIRECTORY_SEPARATOR . "acl/acl.cache";
         
         /**
          *
          * @var \Zend\Permissions\Acl\Acl;
          */
-        protected $acl;
+        protected $acl = NULL;
         
-        public function __construct()
+        /**
+         * The single instance of the ACL.
+         *
+         * @var \Alder\Acl\Acl
+         */
+        protected static $instance = NULL;
+        
+        /**
+         * 
+         * @return \Alder\Acl\Acl The created instance of the Acl.
+         */
+        public static function create()
         {
+            if (!isset(self::$instance)) {
+                self::$instance = new self();
+            }
+            return self::$instance;
+        }
+        
+        /**
+         * Prepares the ACL object, fetching from the filesystem if cached, constructing from default settings otherwise.
+         */
+        protected function __construct() {
+            // TODO(Matthew): Measure performance metrics of unserialize vs reconstructing ACL each time.
             // Retrieve ACL from filesystem if it exists and is valid.
             if (file_exists(self::ACL_CACHE)) {
                 $acl = unserialize(file_get_contents(self::ACL_CACHE));
                 if (!$acl instanceof Acl) {
-                    $this->acl = new Acl();
+                    $this->acquireDefaultAcl();
                 } else {
                     $this->acl = $acl;
                 }
             } else {
-                $this->acl = new Acl();
+                $this->acquireDefaultAcl();
             }
-            
+        }
+        
+        /**
+         * Fetches the current ACL object.
+         * 
+         * @return \Zend\Permissions\Acl\Acl The current ACL object.
+         */
+        public function get()
+        {
+            return $this->acl;
+        }
+        
+        protected function acquireDefaultAcl()
+        {
             
         }
     }

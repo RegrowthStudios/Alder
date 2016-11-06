80f2356fd966189eac0b45e1d00f7d0cd1197b60
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-11-06 06:24:23 +0000

    Rename Container to AclContainer.

---
+++ alder_public_authentication/src/Alder/Acl/AclContainer.php
@@ -0,0 +1,31 @@
+<?php
+
+    namespace Alder\Acl;
+    
+    use Alder\Stdlib\Container as StdContainer;
+
+    use Zend\Permissions\Acl\Acl;
+
+    /**
+     * Provides functionality for retrieving and storing ACL data as well as checking access rights of users against it.
+     * 
+     * @author Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
+     * @since 0.1.0
+     */
+    class Container extends StdContainer
+    {
+        /**
+         * Filepath to configuration for the default ACL object.
+         */
+        const DEFAULT_ACL_FILEPATH = CONFIG_DIRECTORY . DIRECTORY_SEPARATOR . "acl" . DIRECTORY_SEPARATOR . "acl.default.php";
+        
+        /**
+         * Filepath to cache for the custom ACL object.
+         */
+        const CUSTOM_ACL_FILEPATH = CACHE_DIRECTORY . DIRECTORY_SEPARATOR . "acl" . DIRECTORY_SEPARATOR . "acl.cache";
+        
+        public static function create() {
+            return parent::create(self::DEFAULT_ACL_FILEPATH, self::CUSTOM_ACL_FILEPATH);
+        }
+    }

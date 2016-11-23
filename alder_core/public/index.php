759b1b9d11dd3c80746e1e3eec7a1c42284ea3b4
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-11-23 00:57:59 +0000

    Require all scripts in global directory.

--- alder_core/public/index.php
+++ alder_core/public/index.php
@@ -22,7 +22,11 @@
         return false;
     }
     
-    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "global" . DIRECTORY_SEPARATOR . "global.php";
+    // Require global scripts.
+    foreach (glob(dirname(__DIR__) . DIRECTORY_SEPARATOR . "global" . DIRECTORY_SEPARATOR . "*.php") as $filename) {
+        require_once $filename;
+    }
+    // Require constants definition files.
     foreach (glob(file_build_path(dirname(__DIR__), "config", "constants", "*.php")) as $filename) {
         require_once $filename;
     }

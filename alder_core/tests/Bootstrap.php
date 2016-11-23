22cd5d70999557f457de18d3631260d3243efa5f
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-11-23 01:13:58 +0000

    Correct bootstrap for tests to require correct global scripts and constants definitions.

--- alder_core/tests/Bootstrap.php
+++ alder_core/tests/Bootstrap.php
@@ -2,8 +2,14 @@
     namespace AlderTest;
     
     error_reporting(E_ALL | E_STRICT);
-    
-    require dirname(__DIR__) . DIRECTORY_SEPARATOR . "global.php";
-    require file_build_path(dirname(__DIR__), "config", "constants.php");
+
+    // Require global scripts.
+    foreach (glob(dirname(__DIR__) . DIRECTORY_SEPARATOR . "global" . DIRECTORY_SEPARATOR . "*.php") as $filename) {
+        require_once $filename;
+    }
+    // Require constants definition files.
+    foreach (glob(file_build_path(dirname(__DIR__), "config", "constants", "*.php")) as $filename) {
+        require_once $filename;
+    }
     
     require file_build_path(VENDOR_DIRECTORY, "autoload.php");

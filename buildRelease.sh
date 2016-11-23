bc2af2237730edd3e447b4f4b30f11d6cb4a7694
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-11-23 00:59:11 +0000

    Neaten up build directory, make loose files build temporary, and run tests on loose files build.

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+if (( $# < 1 )); then
+    echo "Must supply the name of the component(s) (less the 'alder_' prefix) to include in the build."
+    exit 1
+fi
+
 echo "Beginning build process..."
 
 if [ ! -d "build" ]; then
     mkdir "build"
+fi
+if [ ! -d "build/artefacts" ]; then
+    mkdir "build/artefacts"
+fi
+if [ ! -d "build/test_results" ]; then
+    mkdir "build/test_results"
+fi
+if [ ! -d "build/tmp" ]; then
+    mkdir "build/tmp"
 fi
 
 component_str=""
@@ -29,43 +43,64 @@ done
 echo
 echo "Building release with component(s): ${component_str}..."
 
-build_dir="build/$build_name"
+tmp_dir="build/tmp"
+tmp_build_dir="${tmp_dir}/$build_name"
+test_results_dir="build/test_results/$build_name"
 
-if [ -d "$build_dir" ]; then
-    rm -r "$build_dir"
+if [ -d "$tmp_build_dir" ]; then
+    rm -r "$tmp_build_dir"
 fi
-mkdir "$build_dir"
+mkdir "$tmp_build_dir"
 
 echo
 echo "Copying over third-party libraries..."
-cp -r vendor/. "${build_dir}/vendor"
+cp -r vendor/. "${tmp_build_dir}/vendor"
 
 echo "Copying over core library..."
-cp -r alder_core/config/. "${build_dir}/config" 2>/dev/null
-cp -r alder_core/global/. "${build_dir}/global" 2>/dev/null
-cp -r alder_core/public/. "${build_dir}/public" 2>/dev/null
-cp -r alder_core/src/. "${build_dir}/src" 2>/dev/null
+cp alder_core/phpunit.xml "${tmp_build_dir}/phpunit.xml" 2>/dev/null
+cp -r alder_core/config/. "${tmp_build_dir}/config" 2>/dev/null
+cp -r alder_core/global/. "${tmp_build_dir}/global" 2>/dev/null
+cp -r alder_core/public/. "${tmp_build_dir}/public" 2>/dev/null
+cp -r alder_core/src/. "${tmp_build_dir}/src" 2>/dev/null
+cp -r alder_core/tests/. "${tmp_build_dir}/src" 2>/dev/null
 
 for v in "$@"
 do
     echo "Copying over $v component..."
-    cp -r "alder_${v}/apimap/." "${build_dir}/apimap" 2>/dev/null
-    cp -r "alder_${v}/config/." "${build_dir}/config" 2>/dev/null
-    cp -r "alder_${v}/global/." "${build_dir}/global" 2>/dev/null
-    cp -r "alder_${v}/languages/." "${build_dir}/languages" 2>/dev/null
-    cp -r "alder_${v}/public/." "${build_dir}/public" 2>/dev/null
-    cp -r "alder_${v}/src/." "${build_dir}/src" 2>/dev/null
+    cp -r "alder_${v}/apimap/." "${tmp_build_dir}/apimap" 2>/dev/null
+    cp -r "alder_${v}/config/." "${tmp_build_dir}/config" 2>/dev/null
+    cp -r "alder_${v}/global/." "${tmp_build_dir}/global" 2>/dev/null
+    cp -r "alder_${v}/languages/." "${tmp_build_dir}/languages" 2>/dev/null
+    cp -r "alder_${v}/public/." "${tmp_build_dir}/public" 2>/dev/null
+    cp -r "alder_${v}/src/." "${tmp_build_dir}/src" 2>/dev/null
+    cp -r "alder_${v}/tests/." "${tmp_build_dir}/tests" 2>/dev/null
 done
 
-build_archive_dir="build/${build_name}.tar.gz"
+echo
+echo "Running tests for build..."
+./runTests.sh $tmp_build_dir $test_results_dir
+
+echo
+echo "Creating build archive..."
+curr_dir=$(pwd)
+cd $tmp_dir
+artefacts_dir="../artefacts/$build_name"
+build_archive_dir="${artefacts_dir}/${build_name}.tar.gz"
+
+if [ ! -d $artefacts_dir ]; then
+    mkdir $artefacts_dir
+fi
 
 if [ -f $build_archive_dir ]; then
     rm $build_archive_dir
 fi
 
+tar -cvzf $build_archive_dir $build_name 1>/dev/null
+cd $curr_dir
+
 echo
-echo "Creating build archive..."
-tar -cvzf $build_archive_dir ${build_dir}/ 1>/dev/null
+echo "Clearing up..."
+rm -r $tmp_dir
 
 echo
 echo "Build complete!"

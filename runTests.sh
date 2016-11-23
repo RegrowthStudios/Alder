bdce4bffcb24cb5990e32103203cb0cb47c7e4d9
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-11-23 00:57:37 +0000

    Implement shell script for running tests.

---
+++ runTests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if (( $# != 2 )); then
+    echo "Must supply the directory paths of the build to test and the location to dump test results."
+    exit 1
+fi
+
+phpunit --log-json=$2/test_results.json --coverage-html=$2/coverage/ --coverage-php=$2/coverage.php --testdox-html=$2/test_results.html $1

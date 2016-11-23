13a37809649213a4c79cc1298784c52185dfe688
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-11-23 01:15:33 +0000

    Point phpunit to correct xml file.

--- runTests.sh
+++ runTests.sh
@@ -5,4 +5,4 @@ if (( $# != 2 )); then
     exit 1
 fi
 
-phpunit --log-json=$2/test_results.json --coverage-html=$2/coverage/ --coverage-php=$2/coverage.php --testdox-html=$2/test_results.html $1
+phpunit --log-json=$2/test_results.json --coverage-html=$2/coverage/ --coverage-php=$2/coverage.php --testdox-html=$2/test_results.html --configuration=$1/phpunit.xml $1

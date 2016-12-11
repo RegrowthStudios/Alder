fed9753f5412d8e29c256a6c5257af454a549e41
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-12-11 04:05:18 +0000

    Add skeleton of translate function.

--- alder_core/src/Alder/I18n/Translator/Translator.php
+++ alder_core/src/Alder/I18n/Translator/Translator.php
@@ -2,7 +2,13 @@
     
     namespace Alder\I18n\Translator;
     
-    class Translator implements TranslatorInterface
+    class Translator extends AbstractTranslator
     {
-        
+        /**
+         * {@inheritdoc}
+         */
+        public function translate(string $label, string $domain = self::DEFAULT_DOMAIN,
+                                  string $locale = null) : ?string {
+            
+        }
     }

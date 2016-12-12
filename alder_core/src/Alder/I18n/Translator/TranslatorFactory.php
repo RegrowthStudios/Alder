aec441bf9117c32a9bd64c7d4119d074ae4e17f7
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-12-12 01:13:59 +0000

    Implement factory creation function for Translator object.

--- alder_core/src/Alder/I18n/Translator/TranslatorFactory.php
+++ alder_core/src/Alder/I18n/Translator/TranslatorFactory.php
@@ -1,8 +1,49 @@
 <?php
     
+    namespace Alder\I18n\Translator;
+    
+    use Alder\I18n\Translator\Loader\Loader;
+    
     class TranslatorFactory
     {
-        public static function create() {
-        
+        public static function create(string $defaultLocale, string $defaultBaseDirectory,
+                                      string $fallbackLocale = null, array $files = [], array $filePatterns = [],
+                                      $loaderCache = null) {
+            if (!$loaderCache) {
+                // TODO(Matthew): Prepare default cache object.
+            }
+            
+            $loader = (new Loader())->setCache($loaderCache)
+                                    ->setDefaultBaseDirectory($defaultBaseDirectory);
+            
+            $expectedFileParts = [
+                "filename",
+                "locale"
+            ];
+            foreach ($files as $file) {
+                if (!is_array($file) || !array_key_exists($file, $expectedFileParts)) {
+                    continue;
+                }
+                $loader->addTranslationFile(
+                    $file["filename"],
+                    $file["locale"],
+                    ($file["domain"] ?? Translator::DEFAULT_DOMAIN),
+                    ($file["baseDirectory"] ?? null)
+                );
+            }
+            foreach ($filePatterns as $filePattern) {
+                if (!is_array($filePattern) || !isset($filePattern["pattern"])) {
+                    continue;
+                }
+                $loader->addTranslationFilePattern(
+                    $filePattern["pattern"],
+                    ($filePattern["domain"] ?? Translator::DEFAULT_DOMAIN),
+                    ($filePattern["baseDirectory"] ?? null)
+                );
+            }
+            
+            return (new Translator())->setDefaultLocale($defaultLocale)
+                                     ->setFallbackLocale($fallbackLocale)
+                                     ->setLoader($loader);
         }
     }

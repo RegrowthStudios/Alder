4e69bf8b98f3656ed91ba6691f5bd26c76103705
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2016-12-16 09:14:51 +0000

    Implement tests for convertToString.

--- alder_core/tests/Alder/unit/Stdlib/StringUtilsTest.php
+++ alder_core/tests/Alder/unit/Stdlib/StringUtilsTest.php
@@ -6,19 +6,107 @@
     /**
      * Test functionality of the string utility functions class.
      *
-     * @author Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @author    Matthew Marshall <matthew.marshall96@yahoo.co.uk>
      * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
-     * @since 0.1.0
+     * @since     0.1.0
      */
     class StringUtilsTest extends \PHPUnit_Framework_TestCase
-    {        
+    {
+        /**
+         * Stores a resource stream to temp/test.txt
+         *
+         * @var resource
+         */
+        protected $testFile;
+        
+        /**
+         * Sets up a temporary file and stream for testing purposes.
+         */
+        public function setUp() {
+            file_put_contents(file_build_path(TEMP_DIRECTORY, "test.txt"), "test");
+            
+            $this->testFile = fopen(file_build_path(TEMP_DIRECTORY, "test.txt"), "r");
+        }
+        
         /**
          * @test
-         * 
+         *
+         * @covers \Alder\Stdlib\StringUtils::convertToString
+         */
+        public function convertToStringWorksWithAllDataTypesTest() {
+            $object = new \stdClass();
+            $object->foo = "bar";
+            
+            $data = [
+                1,
+                "hello",
+                2.3,
+                true,
+                null,
+                $object,
+                "test" => 5
+            ];
+            
+            $this->assertTrue(is_string(StringUtils::convertToString($data)));
+            foreach ($data as $datum) {
+                $this->assertTrue(is_string(StringUtils::convertToString($datum)));
+            }
+        }
+        
+        /**
+         * @test
+         *
+         * @covers \Alder\Stdlib\StringUtils::convertToString
+         */
+        public function convertToStringIsDeterministicTest() {
+            $object = new \stdClass();
+            $object->foo = "bar";
+            
+            $data = [
+                1,
+                "hello",
+                2.3,
+                true,
+                null,
+                $object,
+                "test" => 5
+            ];
+            
+            $this->assertEquals(StringUtils::convertToString($data), StringUtils::convertToString($data));
+            foreach ($data as $datum) {
+                $this->assertEquals(StringUtils::convertToString($datum), StringUtils::convertToString($datum));
+            }
+        }
+        
+        /**
+         * @test
+         *
+         * @covers \Alder\Stdlib\StringUtils::convertToString
+         */
+        public function convertToStringThrowsExceptionForResourceTypeTest() {
+            $this->expectException(\InvalidArgumentException::class);
+            
+            StringUtils::convertToString($this->testFile);
+        }
+        
+        /**
+         * @test
+         *
+         * @covers \Alder\Stdlib\StringUtils::convertToString
+         */
+        public function convertToStringThrowsExceptionForUnknownTypeTest() {
+            $this->expectException(\InvalidArgumentException::class);
+            
+            fclose($this->testFile);
+            StringUtils::convertToString($this->testFile);
+        }
+        
+        /**
+         * @test
+         *
          * @covers \Alder\Stdlib\StringUtils::endsWith
          */
-        public function endsWithReturnsTrueIfStringEndsWithProvidedNeedleTest()
-        {
+        public function endsWithReturnsTrueIfStringEndsWithProvidedNeedleTest() {
             $substring = "test";
             $string = "this is a test";
             
@@ -27,11 +115,10 @@
         
         /**
          * @test
-         * 
+         *
          * @covers \Alder\Stdlib\StringUtils::endsWith
          */
-        public function endsWithReturnsFalseIfStringDoesNotEndWithProvidedNeedleTest()
-        {
+        public function endsWithReturnsFalseIfStringDoesNotEndWithProvidedNeedleTest() {
             $substring = "test";
             $string = "this is a test.";
             
@@ -40,11 +127,10 @@
         
         /**
          * @test
-         * 
+         *
          * @covers \Alder\Stdlib\StringUtils::endsWith
          */
-        public function endsWithReturnsFalseForStringShorterThanNeedleTest()
-        {
+        public function endsWithReturnsFalseForStringShorterThanNeedleTest() {
             $substring = "extra long test";
             $string = "test";
             

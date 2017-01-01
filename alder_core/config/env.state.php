257070226925be86b5618926a38508ca089e85e7
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-01-01 20:38:11 +0000

    Remove unneeded configuration files and move base configuration files to installation directory.

--- alder_core/config/env.state.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    /**
-     * Return the environment state to run Alder in.
-     * Possible options are:
-     *  - DEVELOPMENT -- Debugging and timing are fully enabled.
-     *  - PRODUCTION -- No debugging or timing.
-     */
-    return DEVELOPMENT;

9175f069d2ed5545083b519b839e64ede951694e
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-01-03 05:28:21 +0000

    Begin implementing marshaller class.

--- alder_core/install/src/Marshaller/Marshaller.php
+++ alder_core/install/src/Marshaller/Marshaller.php
@@ -2,7 +2,28 @@
     
     namespace Alder\Install\Marshaller;
     
+    use Alder\Install\Info\Cache;
+    
+    use MikeRoetgers\DependencyGraph\DependencyManager;
+    
     class Marshaller
     {
-    
+        public static function marshalInstallAndUpgradeActions() {
+            $dependencyManager = new DependencyManager();
+            
+            foreach (new \DirectoryIterator(INSTALL_DATA_DIRECTORY) as $file) {
+                if ($file->isDir()) {
+                    self::prepareModuleForMarshalling($file->getBasename(), $dependencyManager);
+                }
+            }
+        }
+        
+        protected static function prepareModuleForMarshalling(string $module, DependencyManager& $dependencyManager) {
+            $moduleInfo = Cache::getInfoObj($module);
+            
+            $evaluation = $moduleInfo->evaluateDependencies();
+            if (!$evaluation["_dependenc"]) {
+            
+            }
+        }
     }

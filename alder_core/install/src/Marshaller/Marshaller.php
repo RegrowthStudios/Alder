a95fae43a7b0e9de9273b143cf32c4cdf37340ce
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-01-04 05:23:13 +0000

    Implement dependency evaluation and dependency graph generation stages of marshaller.

--- alder_core/install/src/Marshaller/Marshaller.php
+++ alder_core/install/src/Marshaller/Marshaller.php
@@ -1,11 +1,14 @@
 <?php
     
     namespace Alder\Install\Marshaller;
+
+    use Alder\Install\Module\Cache;
+    use Alder\Install\Module\Module;
     
-    use Alder\Install\Info\Cache;
+    use Composer\Semver\Semver;
     
     use MikeRoetgers\DependencyGraph\DependencyManager;
-    
+
     class Marshaller
     {
         public static function marshalInstallAndUpgradeActions() {
@@ -18,12 +21,59 @@
             }
         }
         
-        protected static function prepareModuleForMarshalling(string $module, DependencyManager& $dependencyManager) {
-            $moduleInfo = Cache::getInfoObj($module);
+        protected static function prepareModuleForMarshalling(string $moduleName, DependencyManager& $dependencyManager) {
+            $module = Cache::getModule($moduleName);
             
-            $evaluation = $moduleInfo->evaluateDependencies();
-            if (!$evaluation["_dependenc"]) {
-            
+            // Evaluate soft dependencies.
+            [ $netSoftEvaluation,
+              $softEvaluations ] = self::evaluateDependencies($module);
+            if (!$netSoftEvaluation) {
+                // TODO(Matthew): Print error using each dependency evaluation.
             }
+            
+            // Evaluate soft dependencies.
+            [ $netHardEvaluation,
+              $hardEvaluations ] = self::evaluateDependencies($module, $dependencyManager);
+            if (!$netHardEvaluation) {
+                // TODO(Matthew): Print error using each dependency evaluation.
+            }
+        }
+        
+        /**
+         * Evaluates the constraints of the dependencies of the module
+         *
+         * @param \Alder\Install\Module\Module $module
+         * @param \MikeRoetgers\DependencyGraph\DependencyManager $dependencyManager
+         *
+         * @return array
+         */
+        protected static function evaluateDependencies(Module $module, DependencyManager& $dependencyManager = null) : array {
+            $netEvaluation = true;
+            $evaluations = [];
+            
+            if ($dependencyManager) {
+                $dependencyManager->addOperation($module);
+            }
+            
+            foreach ($module->getLatestSoftDependencies() as $dependencyName => $constraint) {
+                $dependency = Cache::getModule($dependencyName);
+                
+                $evaluations[$dependencyName] = [
+                    "current_version" => $dependency->getCurrentVersion(),
+                    "future_version" => $dependency->getFutureVersion()
+                ];
+                if (!Semver::satisfies($dependency->getLatestVersion(), $constraint)) {
+                    $netEvaluation = false;
+                    $evaluations[$dependencyName]["evaluation"] = false;
+                } else {
+                    $evaluations[$dependencyName]["evaluation"] = true;
+                    if ($dependencyManager) {
+                        $dependencyManager->addOperation($dependency)
+                                          ->addDependencyByTag($dependency, $module->getModuleName());
+                    }
+                }
+            }
+            
+            return [ $netEvaluation, $evaluations ];
         }
     }

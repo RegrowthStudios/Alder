f1326baf991900d492fddfc453fd4cfc4dd4e9a1
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-07-31 19:54:40 +0100

    Add skeleton for upgrade procedure.

---
+++ alder_core/install/src/Action/Upgrade.php
@@ -0,0 +1,20 @@
+<?php
+    
+    namespace Alder\Install\Action;
+    
+    /**
+     * Class providing the default upgrade behaviour for Alder modules.
+     * 
+     * @author    Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
+     * @since     0.1.0 
+     */
+    class Upgrade implements UpgradeInterface
+    {
+        /**
+         * The default upgrade procedure for Alder modules.
+         */
+        public static function run() {
+            //  TODO(Matthew): Implement default update procedure.
+        }
+    }

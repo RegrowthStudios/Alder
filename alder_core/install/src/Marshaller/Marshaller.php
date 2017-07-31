c0d3d499723927489372424be3887b38d2321e4e
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-07-31 20:07:21 +0100

    Improve documentation of marshaller.

--- alder_core/install/src/Marshaller/Marshaller.php
+++ alder_core/install/src/Marshaller/Marshaller.php
@@ -9,17 +9,30 @@
     
     use MikeRoetgers\DependencyGraph\DependencyManager;
     
+    /**
+     * Class that handles the marshalling of installations and upgrades.
+     * 
+     * @author    Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
+     * @since     0.1.0 
+     */
     class Marshaller
     {
+        /**
+         * Handles the marshalling of the actions required to complete the ongoing installations and/or upgrades.
+         */
         public static function marshalInstallAndUpgradeActions() {
             $dependencyManager = new DependencyManager();
             
+            // Prepare each module for marshalling by entering dependency objects into dependency manager.
             foreach (new \DirectoryIterator(INSTALL_DATA_DIRECTORY) as $file) {
                 if ($file->isDir()) {
                     self::prepareModuleForMarshalling($file->getBasename(), $dependencyManager);
                 }
             }
             
+            // Iterate over all possible executable operations at a given point, until we execute all
+            // operations required by the installation/upgrade process.
             /**
              * @var \Alder\Install\Module\Module[] $executableOperations
              */
@@ -47,17 +60,27 @@
             }
         }
         
+        /**
+         * Prepares the named module for marshalling.
+         *
+         * @param string $moduleName
+         * @param \MikeRoetgers\DependencyGraph\DependencyManager $dependencyManager
+         *
+         * @return array|bool
+         */
         protected static function prepareModuleForMarshalling(string $moduleName, DependencyManager& $dependencyManager) {
             $module = Cache::getModule($moduleName);
             
-            // Evaluate soft dependencies.
+            // Evaluate our capability to satisfy soft dependencies.
+            //   Soft -> we need it to run the app but not to complete this module's install/upgrade.
             [ $netSoftEvaluation,
               $softEvaluations ] = self::evaluateDependencies($module);
             if (!$netSoftEvaluation) {
                 // TODO(Matthew): Print error of each failed dependency evaluation.
             }
             
-            // Evaluate soft dependencies.
+            // Evaluate our capbility to satisfy hard dependencies.
+            //   Hard -> we need it to not just run the app but to complete this module's install/upgrade.
             [ $netHardEvaluation,
               $hardEvaluations ] = self::evaluateDependencies($module, $dependencyManager);
             if (!$netHardEvaluation) {

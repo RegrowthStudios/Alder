0f41b2f1d951f3316e06cfa6ad0419951825f002
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-07-31 19:55:49 +0100

    Pass the module name into the run function.

--- alder_core/install/src/Action/InstallInterface.php
+++ alder_core/install/src/Action/InstallInterface.php
@@ -2,7 +2,14 @@
     
     namespace Alder\Install\Action;
     
+    /**
+     * Interface for all classes providing installation procedures for Alder mdoules.
+     * 
+     * @author    Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
+     * @since     0.1.0 
+     */
     interface InstallInterface
     {
-        public static function run();
+        public static function run(string $moduleName);
     }
--- alder_core/install/src/Action/UpgradeInterface.php
+++ alder_core/install/src/Action/UpgradeInterface.php
@@ -2,6 +2,13 @@
     
     namespace Alder\Install\Action;
     
+    /**
+     * Interface for all classes providing upgrade procedures for Alder mdoules.
+     * 
+     * @author    Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
+     * @since     0.1.0 
+     */
     interface UpgradeInterface
     {
         public static function run();

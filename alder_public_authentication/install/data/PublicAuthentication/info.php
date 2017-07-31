1b21a0453f46fc6fb2f7b6c9f36edd6ec8adefd4
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-07-31 19:59:44 +0100

    Update metadata of Public Authentication modules.

--- alder_public_authentication/install/data/PublicAuthentication/info.php
+++ alder_public_authentication/install/data/PublicAuthentication/info.php
@@ -1,9 +1,12 @@
 <?php
     
     return [
+        "name" => "Alder Public Authentication",
+        "description" => "The public authentication service for Alder applications.",
+        "author" => "Regrowth Studios Ltd.",
         "version" => "0.1.0",
-        "soft_dependencies" => [
+        "soft_dependencies" => [],
+        "hard_dependencies" => [
             "Core" => "^0.1.0"
-        ],
-        "hard_dependencies" => []
+        ]
     ];

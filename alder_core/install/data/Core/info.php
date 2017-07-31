2f67146938ff0658841b6d445813b6bda91b1213
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-07-31 19:51:46 +0100

    Add name, description and author metadata for modules.

--- alder_core/install/data/Core/info.php
+++ alder_core/install/data/Core/info.php
@@ -1,6 +1,9 @@
 <?php
     
     return [
+        "name" => "Alder Core",
+        "description" => "The core library for all other Alder modules.",
+        "author" => "Regrowth Studios Ltd.",
         "version" => "0.1.0",
         "soft_dependencies" => [],
         "hard_dependencies" => []

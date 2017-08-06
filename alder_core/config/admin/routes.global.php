53f27bc828e641f879a13df62913c84aca08679e
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-08-06 07:12:20 +0100

    Add install routes and fix action class paths.

--- alder_core/config/admin/routes.global.php
+++ alder_core/config/admin/routes.global.php
@@ -11,77 +11,112 @@
                 "name" => "dashboard",
                 "path" => "/",
                 "middleware" => [
-                    new \Alder\Install\Action\DashboardAction([ "module" => "core" ])
+                    new \Alder\Admin\Action\DashboardAction([ "module" => "core" ])
                 ]
             ],
             [
                 "name" => "demographics",
                 "path" => "/stats/demographics",
                 "middleware" => [
-                    new \Alder\Install\Action\DemographicsAction([ "module" => "core" ])
+                    new \Alder\Admin\Action\DemographicsAction([ "module" => "core" ])
                 ]
             ],
             [
                 "name" => "demographics",
                 "path" => "/stats/api",
                 "middleware" => [
-                    new \Alder\Install\Action\APIStatisticsAction([ "module" => "core" ])
+                    new \Alder\Admin\Action\APIStatisticsAction([ "module" => "core" ])
                 ]
             ],
             [
                 "name" => "settings",
                 "path" => "/settings",
                 "middleware" => [
-                    new \Alder\Install\Action\SettingsAction([ "module" => "core" ])
+                    new \Alder\Admin\Action\SettingsAction([ "module" => "core" ])
                 ]
             ],
             [
                 "name" => "tasks",
                 "path" => "/tasks",
                 "middleware" => [
-                    new \Alder\Install\Action\TasksAction([ "module" => "core" ])
+                    new \Alder\Admin\Action\TasksAction([ "module" => "core" ])
                 ]
             ],
             [
                 "name" => "cache-rebuilder",
                 "path" => "/cache-rebuilder",
                 "middleware" => [
-                    new \Alder\Install\Action\CacheRebuilderAction([ "module" => "core" ])
+                    new \Alder\Admin\Action\CacheRebuilderAction([ "module" => "core" ])
                 ]
             ],
             [
                 "name" => "file-verifier",
                 "path" => "/file-verifier",
                 "middleware" => [
-                    new \Alder\Install\Action\FileVerifierAction([ "module" => "core" ])
+                    new \Alder\Admin\Action\FileVerifierAction([ "module" => "core" ])
                 ]
             ],
             [
                 "name" => "action-log",
                 "path" => "/action-log",
                 "middleware" => [
-                    new \Alder\Install\Action\ActionLogAction([ "module" => "core" ])
+                    new \Alder\Admin\Action\ActionLogAction([ "module" => "core" ])
                 ]
             ],
             [
                 "name" => "server-error-log",
                 "path" => "/server-error-log",
                 "middleware" => [
-                    new \Alder\Install\Action\ServerErrorLogAction([ "module" => "core" ])
+                    new \Alder\Admin\Action\ServerErrorLogAction([ "module" => "core" ])
                 ]
             ],
             [
                 "name" => "alerts",
                 "path" => "/alerts",
                 "middleware" => [
-                    new \Alder\Install\Action\AlertsAction([ "module" => "core" ])
+                    new \Alder\Admin\Action\AlertsAction([ "module" => "core" ])
                 ]
             ],
             [
                 "name" => "messages",
                 "path" => "/messages",
                 "middleware" => [
-                    new \Alder\Install\Action\MessagesAction([ "module" => "core" ])
+                    new \Alder\Admin\Action\MessagesAction([ "module" => "core" ])
+                ]
+            ],
+            [ // Provides front-end view.
+                "name" => "install",
+                "path" => "/install",
+                "middleware" => [
+                    new \Alder\Admin\Action\InstallAction([ "module" => "core" ])
+                ]
+            ],
+            [ // Provides back-end API.
+                "name" => "install-overview",
+                "path" => "/install/overview",
+                "middleware" => [
+                    new \Alder\Admin\Install\Action\InstallOverviewAction([ "module" => "core" ])
+                ]
+            ],
+            [ // Provides back-end API.
+                "name" => "install-config",
+                "path" => "/install/config",
+                "middleware" => [
+                    new \Alder\Admin\Install\Action\InstallConfigAction([ "module" => "core" ])
+                ]
+            ],
+            [ // Provides back-end API.
+                "name" => "install-database",
+                "path" => "/install/database",
+                "middleware" => [
+                    new \Alder\Admin\Install\Action\InstallDatabaseAction([ "module" => "core" ])
+                ]
+            ],
+            [ // Provides back-end API.
+                "name" => "install-tasks",
+                "path" => "/install/tasks",
+                "middleware" => [
+                    new \Alder\Admin\Install\Action\InstallTasksAction([ "module" => "core" ])
                 ]
             ],
         ]

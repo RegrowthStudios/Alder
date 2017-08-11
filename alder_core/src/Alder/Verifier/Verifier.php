75f064dd355fde5e7ffa61af7d920c82f8db9f01
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-08-11 13:12:53 +0100

    First naive implementation of file verification. (Does not account for dependencies overwriting one another.)

---
+++ alder_core/src/Alder/Verifier/Verifier.php
@@ -0,0 +1,46 @@
+<?php
+
+    namespace Alder\Verifier;
+
+    class Verifier
+    {
+        const NO_MANIFEST = "no_manifest";
+        const NOT_VALID   = "not_valid";
+        const VALID       = "valid";
+
+        protected static function verifyAllInDir(string $directory, string $manifest = "files.json") : array {
+            $results = [];
+
+            foreach (\DirectoryIterator($directory) as $file) {
+                if (!$file->isDir()) continue;
+
+                $name = $file->getBasename();
+                $manifestFilepath = file_build_path($file->getPathname(), $manifest);
+
+                if (!file_exists($manifestFilepath)) {
+                    $results[$name]["result"] = NO_MANIFEST;
+                    continue;
+                }
+
+                $results[$name]["result"] = VALID;
+
+                $manifestData = json_decode(file_get_contents($manifestFilepath));
+
+                foreach ($manifestData["src"] as $filepathPart => $hash) {
+                    $filepath = file_build_path(SRC_DIRECTORY, $filepathPart);
+                    if (!file_exists($filepath)) {
+                        $results[$name]["result"] = NOT_VALID;
+                        $results[$name]["files"][$filepath] = NOT_VALID;
+                        continue;
+                    }
+
+                    if ($hash == md5(file_get_contents($filepath))) {
+                        $results[$name]["files"][$filepath] = VALID;
+                    } else {
+                        $results[$name]["result"] = NOT_VALID;
+                        $results[$name]["files"][$filepath] = NOT_VALID;
+                    }
+                }
+            }
+        }
+    }

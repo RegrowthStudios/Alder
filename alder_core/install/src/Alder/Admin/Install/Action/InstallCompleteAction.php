63b649000828ab737dd33859872a50222cc7127a
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-08-11 11:55:27 +0100

    Create install complete action skeleton.

---
+++ alder_core/install/src/Alder/Admin/Install/Action/InstallCompleteAction.php
@@ -0,0 +1,68 @@
+<?php
+    
+    namespace Alder\Admin\Install\Action;
+
+    use \MikeRoetgers\DependencyGraph\DependencyManager;
+
+    use \Twig_Environment;
+    use \Twig_Loader_Filesystem;
+
+    use \Zend\Expressive\Twig\TwigRenderer;
+    use \Zend\Diactoros\Response\HtmlResponse;
+
+    /**
+     * The install complete action for showing complete installation view.
+     *
+     * @author    Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
+     * @since     0.1.0
+     */
+    class InstallCompleteAction
+    {
+        /**
+         * The request sent by the client.
+         *
+         * @var \Psr\Http\Message\ServerRequestInterface
+         */
+        protected $request;
+        
+        /**
+         * The response to be delivered to the request sender.
+         *
+         * @var \Psr\Http\Message\ResponseInterface
+         */
+        protected $response;
+
+        /**
+         * Determines the appropriate action function to call for the request method and parameters.
+         *
+         * @param \Psr\Http\Message\ServerRequestInterface $request  The request object.
+         * @param \Psr\Http\Message\ResponseInterface      $response The response object.
+         * @param callable                                 $next     The next middleware to be called.
+         *
+         * @return NULL|\Psr\Http\Message\ResponseInterface
+         */
+        public function __invoke(ServerRequestInterface $request, ResponseInterface $response,
+                                 callable $next = null) : ?ResponseInterface {
+            $this->request = $request;
+            $this->response = $response;
+
+            switch (strtoupper($this->request->getMethod())) {
+                case "GET":
+                    showComplete();
+                    break;
+                default:
+                    $this->response = $this->response->withStatus(405);
+            }
+
+            if ($next) {
+                return $next($this->request, $this->response);
+            }
+            
+            return $this->response;
+        }
+
+        protected function showComplete() : void {
+
+        }
+    }

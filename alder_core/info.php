b18f419cb81917071d9458a5167466cb70c35925
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-08-12 09:27:04 +0100

    Add URLs for author and module.

--- alder_core/info.php
+++ alder_core/info.php
@@ -4,7 +4,9 @@
         "name" => "Alder Core", // Human-readable name of component.
         "namespace" => "Core", // Namespace(/directory) to put component-specific code inside.
         "description" => "The core library for all other Alder modules.", // Description of component.
+        "url" => "www.example.com", // URL of component.
         "author" => "Regrowth Studios Ltd.", // Author of component.
+        "author_url" => "www.regrowthstudios.com", // URL of author.
         "version" => "0.1.0", // Version of this instance of component.
         "soft_dependencies" => [], // Dependencies required after installation of this component.
         "hard_dependencies" => [] // Dependencies required before  installation of this component.

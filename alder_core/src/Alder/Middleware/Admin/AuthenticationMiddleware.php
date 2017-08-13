638a36150910005ba9dfaaf5ed15c37fe0e46148
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-08-13 20:56:56 +0100

    Check for correct install path, and redirect to login on GET and 403 on other HTTP methods.

--- alder_core/src/Alder/Middleware/Admin/AuthenticationMiddleware.php
+++ alder_core/src/Alder/Middleware/Admin/AuthenticationMiddleware.php
@@ -34,20 +34,34 @@
          * @return \Psr\Http\Message\ResponseInterface The response produced.
          */
         public function call(callable $next = null) : ResponseInterface {
-            if (!DiContainer::get()->get("config")->alder->installed ||
-                $request->getUri()->getPath() == "/admin/login") {
-                return $next($request, $response);
+            $config = DiContainer::get()->get("config")->alder;
+            $installed      = $config->installed;
+            $adminRouteRoot = $config->admin->route_root;
+
+            if (!$installed || $this->request->getUri()->getPath() == "/$adminRouteRoot/login") {
+                return $next($this->request, $this->response);
             }
 
             // Get cookies.
-            $cookies = $request->getCookieParams();
+            $cookies = $this->request->getCookieParams();
             // Get most authoratative session token.
             $sessionTokenString = $this->getParameter(ADMIN_SESSION, $cookies[ADMIN_SESSION] ?? null);
 
             if (!$sessionTokenString) {
-                // TODO(Matthew): Handle this by redirecting to /admin/install on GET and 403 on other method if admin is not obfuscated.
-                //                If admin is obfuscated, show 404.
-                return new EmptyResponse(403);
+                if (strtoupper($this->request->getMethod()) == "GET") {
+                    // TODO(Matthew): Should we instead show a login view with a "danger" dialog but without redirecting?
+                    /*
+                     * @var \Zend\Expressive\Helper\ServerUrlHelper $uri
+                     */
+                    $uri = DiContainer::get()->get(\Zend\Expressive\Helper\ServerUrlHelper::class);
+    
+                    $adminRouteRoot = DiContainer::get()->get("config")->alder->admin->route_root;
+                    
+                    header("Location: " . $uri("/$adminRouteRoot/login"), true, 303);
+                    exit();
+                } else {
+                    return new EmptyResponse(403);
+                }
             }
 
             // TODO(Matthew): Create admin session token generator/validator class.
@@ -62,9 +76,22 @@
             // TODO(Matthew): Result fail may return a string rather than false.
             //                (Validator is incomplete.)
             if (!$result) {
-                return new EmptyResponse(403);
+                if (strtoupper($this->request->getMethod()) == "GET") {
+                    // TODO(Matthew): Should we instead show a login view with a "danger" dialog but without redirecting?
+                    /*
+                     * @var \Zend\Expressive\Helper\ServerUrlHelper $uri
+                     */
+                    $uri = DiContainer::get()->get(\Zend\Expressive\Helper\ServerUrlHelper::class);
+    
+                    $adminRouteRoot = DiContainer::get()->get("config")->alder->admin->route_root;
+
+                    header("Location: " . $uri("/$adminRouteRoot/login"), true, 303);
+                    exit();
+                } else {
+                    return new EmptyResponse(403);
+                }
             }
 
-            return $next($request, $response);
+            return $next($this->request, $this->response);
         }
     }

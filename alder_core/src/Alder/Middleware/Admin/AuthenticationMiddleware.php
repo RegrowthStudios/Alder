7834a03d97cbd541c472f9e8b7e8b00e5cee66e5
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-08-13 15:32:08 +0100

    Create authentication middleware for admin routes.

---
+++ alder_core/src/Alder/Middleware/Admin/AuthenticationMiddleware.php
@@ -0,0 +1,69 @@
+<?php
+    
+    namespace Alder\Middleware\Admin;
+    
+    use Alder\DiContainer;
+    use Alder\Token\Validator;
+    
+    use Lcobucci\JWT\Parser;
+
+    use Psr\Http\Message\ResponseInterface;
+    use Psr\Http\Message\ServerRequestInterface;
+    
+    use Zend\Diactoros\Uri;
+    use Zend\Diactoros\Response\EmptyResponse;
+    use Zend\Stratigility\MiddlewareInterface;
+    
+    /**
+     * Determines if requestee is authenticated sufficiently to access admin section of application.
+     *
+     * @author    Matthew Marshall <matthew.marshall96@yahoo.co.uk>
+     * @copyright 2016, Regrowth Studios Ltd. All Rights Reserved
+     * @since     0.1.0
+     */
+    class InstallNeededMiddleware implements MiddlewareInterface
+    {
+        /**
+         * Determine if requestee is authenticated sufficiently to access admin section of application.
+         *
+         * @param \Psr\Http\Message\ServerRequestInterface $request  The data of the request.
+         * @param \Psr\Http\Message\ResponseInterface      $response The response to be sent back.
+         * @param callable|NULL                            $next     The next middleware to be called.
+         *
+         * @return \Psr\Http\Message\ResponseInterface The response produced.
+         */
+        public function __invoke(ServerRequestInterface $request, ResponseInterface $response,
+                                 callable $next = null) : ResponseInterface {
+            if (!DiContainer::get()->get("config")->alder->installed) {
+                return $next($request, $response);
+            }
+
+            // Get cookies.
+            $cookies = $request->getCookieParams();
+            // Get most authoratative session token.
+            $sessionTokenString = $this->getParameter(ADMIN_SESSION, $cookies[ADMIN_SESSION] ?? null);
+
+            if (!$sessionTokenString) {
+                // TODO(Matthew): Handle this by redirecting to /admin/install on GET and 403 on other method if admin is not obfuscated.
+                //                If admin is obfuscated, show 404.
+                return new EmptyResponse(403);
+            }
+
+            // TODO(Matthew): Create admin session token generator/validator class.
+            $sessionToken = (new Parser())->parse($sessionTokenString);
+
+            $result = Validator::validate($sessionToken, [
+                "public" => [
+                    "sub" => "admin"
+                ]
+            ]);
+
+            // TODO(Matthew): Result fail may return a string rather than false.
+            //                (Validator is incomplete.)
+            if (!$result) {
+                return new EmptyResponse(403);
+            }
+
+            return $next($request, $response);
+        }
+    }
